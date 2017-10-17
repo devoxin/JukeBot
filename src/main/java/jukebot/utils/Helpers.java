82b0cc994141e064b8425e10e018d754fb7e5290
Devoxin <lukel9914@gmail.com>
2017-10-17 11:10:42 +0100

    Optimize imports

--- src/main/java/jukebot/utils/Helpers.java
+++ src/main/java/jukebot/utils/Helpers.java
@@ -3,15 +3,11 @@ package jukebot.utils;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.managers.AudioManager;
 
-import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-
-import static jukebot.utils.Bot.LOG;
 
 public class Helpers {
     private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
