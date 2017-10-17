168fbc28e06d609b666135a08f3ddd2da95fc995
Devoxin <lukel9914@gmail.com>
2017-10-17 11:58:46 +0100

    Remove unused import

--- src/main/java/jukebot/utils/Helpers.java
+++ src/main/java/jukebot/utils/Helpers.java
@@ -8,7 +8,6 @@ import net.dv8tion.jda.core.managers.AudioManager;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class Helpers {
     private static ExecutorService executor = Executors.newSingleThreadExecutor();
