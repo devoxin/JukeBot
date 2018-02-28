0de3763c0ea75d9c13f442ccbc8900f1aa49b353
Devoxin <lukel9914@gmail.com>
2018-02-28 03:00:38 +0000

    remove comment

--- src/main/java/jukebot/audioutilities/PornHubAudioTrack.java
+++ src/main/java/jukebot/audioutilities/PornHubAudioTrack.java
@@ -1,11 +1,14 @@
 package jukebot.audioutilities;
 
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerProbe;
 import com.sedmelluq.discord.lavaplayer.container.mpeg.MpegAudioTrack;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
+import com.sedmelluq.discord.lavaplayer.tools.io.PersistentHttpStream;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import com.sedmelluq.discord.lavaplayer.track.DelegatedAudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.InternalAudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor;
 
 import java.net.URI;
@@ -39,7 +42,6 @@ public class PornHubAudioTrack extends DelegatedAudioTrack {
 
     private void processStatic(LocalAudioTrackExecutor localExecutor, HttpInterface httpInterface) throws Exception {
         try (PornHubPersistentHttpStream stream = new PornHubPersistentHttpStream(httpInterface, new URI(trackInfo.identifier), null)) {
-            //processDelegate(new MatroskaAudioTrack(trackInfo, stream), localExecutor);
             processDelegate(new MpegAudioTrack(trackInfo, stream), localExecutor);
         }
     }
