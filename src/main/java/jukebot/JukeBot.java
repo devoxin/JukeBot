922071788a70afe326fbd9229ca43435131fb63c
Devoxin <lukel9914@gmail.com>
2018-06-06 22:52:43 +0100

    Bump version, newline

--- src/main/java/jukebot/JukeBot.java
+++ src/main/java/jukebot/JukeBot.java
@@ -44,7 +44,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class JukeBot {
 
     /* Bot-Related*/
-    public static final String VERSION = "6.2.2";
+    public static final String VERSION = "6.3.0";
     public static final Long startTime = System.currentTimeMillis();
     public static boolean isReady = false;
     public static Logger LOG = LoggerFactory.getLogger("JukeBot");
