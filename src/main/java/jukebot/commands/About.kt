6356ebbb02da2683e29f38e854ed7ad0ae89885b
Devoxin <lukel9914@gmail.com>
2018-06-22 17:52:58 +0100

    Fix NPE

---
+++ src/main/java/jukebot/commands/About.kt
@@ -0,0 +1,27 @@
+package jukebot.commands
+
+import com.sedmelluq.discord.lavaplayer.tools.PlayerLibrary
+import jukebot.JukeBot
+import jukebot.utils.Command
+import jukebot.utils.CommandProperties
+import jukebot.utils.Context
+import net.dv8tion.jda.core.JDAInfo
+import net.dv8tion.jda.core.entities.MessageEmbed
+import org.sqlite.SQLiteJDBCLoader
+
+@CommandProperties(aliases = ["info"], description = "Displays some information about the bot")
+class About : Command {
+
+    override fun execute(context: Context) {
+        val dependencies = "**JDA**: ${JDAInfo.VERSION}"
+                "\n**Lavaplayer**: ${PlayerLibrary.VERSION}"
+                "\n**SQLite**: ${SQLiteJDBCLoader.getVersion()}"
+
+        val fields = arrayOf(
+                MessageEmbed.Field("Dependencies", dependencies, true),
+                MessageEmbed.Field("Links", "[GitHub](https://github.com/Devoxin/JukeBot)\n[Website](https://jukebot.xyz)", true)
+        )
+
+        context.sendEmbed("About JukeBot ${JukeBot.VERSION}", "Developer: **Kromatic#0420**", fields)
+    }
+}
