2302faa82ab351f567c2cd815a85ac1da8e8c44c
Devoxin <lukel9914@gmail.com>
2018-07-20 22:34:11 +0100

    blame discord

--- src/main/java/jukebot/commands/Dev.java
+++ src/main/java/jukebot/commands/Dev.java
@@ -5,6 +5,7 @@ import jukebot.JukeBot;
 import jukebot.utils.Command;
 import jukebot.utils.CommandProperties;
 import jukebot.utils.Context;
+import net.dv8tion.jda.core.entities.Guild;
 
 import java.util.Random;
 
@@ -50,8 +51,29 @@ public class Dev implements Command {
                 Database.unblockUser(Long.parseLong(args[1]));
                 context.sendEmbed("User Unblocked", args[1] + " can now use JukeBot.");
             }
+        } else if (args[0].equalsIgnoreCase("forcedc")) {
+            if (args.length < 2) {
+                context.sendEmbed("Missing Required Arg", "You need to specify `guildId`");
+            }
+
+            final Guild g = JukeBot.shardManager.getGuildById(args[1]);
+            if (g == null) {
+                context.sendEmbed("Unable to find Guild", "No guilds found matching that ID");
+            } else {
+                g.getAudioManager().closeAudioConnection();
+            }
+        } else if (args[0].equalsIgnoreCase("forcerc")) {
+            if (args.length < 2) {
+                context.sendEmbed("Missing Required Arg", "You need to specify `guildId`");
+            }
+
+            if (context.getMember().getVoiceState().getChannel() == null) {
+                context.sendEmbed("Join VoiceChannel", "This command requires you to be in a voicechannel");
+            }
+
+            context.getGuild().getAudioManager().openAudioConnection(context.getMember().getVoiceState().getChannel());
         } else {
-            context.sendEmbed("Dev Subcommands", "`->` preload <key>\n`->` block <userId>\n`->` unblock <userId>", randomQuote());
+            context.sendEmbed("Dev Subcommands", "`->` preload <key>\n`->` block <userId>\n`->` unblock <userId>\n`->` forcedc <guildId>\n`->` forcerc", randomQuote());
         }
     }
 
