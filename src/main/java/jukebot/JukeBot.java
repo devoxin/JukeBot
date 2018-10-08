6e5fe186f0f9f897c9df6eddb9575f5e01a13dd9
Devoxin <lukel9914@gmail.com>
2018-10-08 16:25:02 +0100

    hopefully this works

--- src/main/java/jukebot/JukeBot.java
+++ src/main/java/jukebot/JukeBot.java
@@ -45,6 +45,7 @@ public class JukeBot {
 
     /* Bot-Related*/
     public static final String VERSION = "6.3.3";
+
     public static final Long startTime = System.currentTimeMillis();
     public static boolean isReady = false;
     public static Logger LOG = LoggerFactory.getLogger("JukeBot");
