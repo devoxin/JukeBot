47ee2a38ee96b7a04f871d3598d146c256d5040b
Devoxin <lukel9914@gmail.com>
2018-12-20 15:36:38 +0000

    Eval :^)

---
+++ src/main/java/jukebot/commands/Eval.kt
@@ -0,0 +1,36 @@
+package jukebot.commands
+
+import jukebot.Database
+import jukebot.JukeBot
+import jukebot.utils.Command
+import jukebot.utils.CommandProperties
+import jukebot.utils.Context
+import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
+
+@CommandProperties(description = "Evaluate arbitrary code.", developerOnly = true)
+class Eval : Command {
+
+    private val engine = KotlinJsr223JvmLocalScriptEngineFactory().scriptEngine
+    private val db = Database()
+
+    override fun execute(context: Context) {
+        val bindings = mapOf(
+                "ctx" to context,
+                "jda" to context.jda,
+                "sm" to JukeBot.shardManager,
+                "db" to db
+        )
+
+        val bindString = bindings.map { "val ${it.key} = bindings[\"${it.key}\"] as ${it.value.javaClass.kotlin.qualifiedName}" }.joinToString("\n")
+        val bind = engine.createBindings()
+        bind.putAll(bindings)
+
+        try {
+            val result = engine.eval("$bindString\n${context.argString}", bind)
+            context.channel.sendMessage("```\n$result```").queue()
+        } catch (e: Exception) {
+            context.channel.sendMessage("Engine Error\n```\n$e```").queue()
+        }
+    }
+
+}
