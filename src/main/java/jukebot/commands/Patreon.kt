301207f980c3e6204e6ef20fe48623fa1872616e
Devoxin <lukel9914@gmail.com>
2019-09-19 22:26:07 +0100

    :blobsweats:

--- src/main/java/jukebot/commands/Patreon.kt
+++ src/main/java/jukebot/commands/Patreon.kt
@@ -9,6 +9,11 @@ import net.dv8tion.jda.api.entities.MessageEmbed
 @CommandProperties(aliases = ["donate"], description = "Provides a link to JukeBot's Patreon")
 class Patreon : Command(ExecutionType.STANDARD) {
 
+    private val BASE_URL = "https://www.patreon.com/join/devoxin/checkout?rid="
+    private val TIER_1 = "2127364"
+    private val TIER_2 = "1594794"
+    private val TIER_3 = "4008303"
+
     override fun execute(context: Context) {
         val fields = arrayOf(
                 MessageEmbed.Field("Tier 0 (Free)",
@@ -20,12 +25,19 @@ class Patreon : Command(ExecutionType.STANDARD) {
                                 "• Ability to queue songs up to 5 hours long\n" +
                                 "• Ability to queue up to 1000 songs from a playlist\n" +
                                 "• Ability to queue livestreams\n" +
-                                "• Up to 50 custom playlists", false),
+                                "• Up to 50 custom playlists\n" +
+                                "[Join Tier]($BASE_URL$TIER_1)\n\u200b", false),
                 MessageEmbed.Field("Tier 2 ($2)",
                         "• **Everything in Tier 1 and...**\n" +
                                 "• Unlimited song duration\n" +
                                 "• Unlimited tracks from a playlist\n" +
-                                "• Spotify playlist support", false)
+                                "• Spotify playlist support\n" +
+                                "[Join Tier]($BASE_URL$TIER_2)\n\u200b", false),
+                MessageEmbed.Field("Tier 3 ($3)",
+                        "• **Everything in Tier 2 and...**\n" +
+                                "• Register premium servers which grant all members access to Tier 2 level perks\n" +
+                                "• Multi-server discounts; 50% off the cost of each additional server\n" +
+                                "[Join Tier]($BASE_URL$TIER_3)", false)
         )
 
         context.embed {
