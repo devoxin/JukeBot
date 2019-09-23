c2174dd72eb5d3dc081792c66fe6e4369973ade9
Devoxin <lukel9914@gmail.com>
2019-09-23 22:41:59 +0100

    Should yield slightly more accurate results

--- src/main/java/jukebot/apis/spotify/SpotifyAudioTrack.kt
+++ src/main/java/jukebot/apis/spotify/SpotifyAudioTrack.kt
@@ -1,13 +1,18 @@
 package jukebot.apis.spotify
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import jukebot.JukeBot
 import org.json.JSONObject
+import java.util.concurrent.CompletableFuture
 
 data class SpotifyAudioTrack(
     val title: String,
     val artist: String
 ) {
-    fun toYoutubeAudioTrack() = JukeBot.youTubeApi.search("$title $artist")
+    fun toYoutubeAudioTrack(): CompletableFuture<AudioTrack> {
+        val append = if (title.contains("remix")) "" else "-remix"
+        return JukeBot.youTubeApi.search("$title $artist $append")
+    }
 
     companion object {
         fun fromJson(json: JSONObject): SpotifyAudioTrack {
