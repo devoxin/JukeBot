da6779a4001a4870ada107f93fef0316199324b3
Devoxin <lukel9914@gmail.com>
2019-11-09 18:05:57 +0000

    Bump jda ver

--- src/main/java/jukebot/JukeBot.java
+++ src/main/java/jukebot/JukeBot.java
@@ -54,7 +54,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class JukeBot {
 
     /* Bot-Related*/
-    public static final String VERSION = "6.5.4";
+    public static final String VERSION = "6.5.5";
 
     public static final Long startTime = System.currentTimeMillis();
     public static boolean isReady = false;
