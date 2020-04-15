b9c89632cd1c9a24fbe5deaacb70339b4e968dce
Devoxin <lukel9914@gmail.com>
2020-04-15 18:25:36 +0100

    Small change to KsoftApi

--- src/main/java/jukebot/apis/ksoft/KSoftAPI.kt
+++ src/main/java/jukebot/apis/ksoft/KSoftAPI.kt
@@ -11,45 +11,32 @@ import java.util.concurrent.CompletableFuture
 
 class KSoftAPI(private val key: String) {
     fun getMusicRecommendations(vararg tracks: String): CompletableFuture<String> {
-        val fut = CompletableFuture<String>()
-
         val obj = JsonWriter.string()
             .`object`()
             .value("provider", "youtube_titles")
-            .value("type", "youtube_id")
+            .value("recommend_type", "youtube_id")
             .value("limit", 1)
             .value("tracks", tracks)
 
-        if ("youtube" in JukeBot.config) {
-            obj.value("youtube_token", JukeBot.config["youtube"])
-        }
+        //if ("youtube" in JukeBot.config) {
+            //obj.value("youtube_token", JukeBot.config["youtube"])
+        //}
 
         val jsonString = obj.end().done()
 
-        makeRequest("/music/recommendations") {
-            post(RequestBody.create(applicationJson, jsonString))
-        }.thenAccept {
-            val results = it.getArray("tracks")
+        return makeRequest("/music/recommendations") { post(RequestBody.create(applicationJson, jsonString)) }
+            .thenApply {
+                val results = it.getArray("tracks")
 
-            if (results.size == 0) {
-                fut.completeExceptionally(IllegalStateException("No recommendations were returned by KSoft API"))
-                return@thenAccept
+                if (results.size == 0) {
+                    throw IllegalStateException("No recommendations were returned by KSoft API")
+                }
+
+                val first = results.getObject(0)
+                val youtube = first["youtube"]
+
+                if (youtube is String) youtube else (youtube as JsonObject).getString("id")
             }
-
-            val first = results.getObject(0)
-            val youtube = first["youtube"]
-
-            if (youtube is JsonObject) {
-                fut.complete(youtube.getString("id"))
-            } else {
-                fut.complete(youtube as String)
-            }
-        }.exceptionally {
-            fut.completeExceptionally(it)
-            return@exceptionally null
-        }
-
-        return fut
     }
 
     fun makeRequest(endpoint: String, requestOptions: (Request.Builder.() -> Unit)? = null): CompletableFuture<JsonObject> {
@@ -57,9 +44,7 @@ class KSoftAPI(private val key: String) {
             .request {
                 url(BASE_URL + endpoint)
                 header("Authorization", "Bearer $key")
-                requestOptions?.let {
-                    apply(requestOptions)
-                }
+                requestOptions?.let { this.apply(it) }
             }
             .submit()
             .thenApply {
