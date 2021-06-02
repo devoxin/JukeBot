bbde12a0cbafbbfc06e92c5ca65560db75a662c9
Stonedestroyer <1307729+Stonedestroyer@users.noreply.github.com>
2021-06-02 20:12:37 +0200

    Pornhub scraping fix

--- src/main/java/jukebot/audio/sourcemanagers/pornhub/Utils.kt
+++ src/main/java/jukebot/audio/sourcemanagers/pornhub/Utils.kt
@@ -1,13 +1,12 @@
 package jukebot.audio.sourcemanagers.pornhub
 
 object Utils {
-    //private val assignmentPattern = "(var.+?media_0[^<]+)".toPattern()
-    //private val assignmentPattern = "(var.+?media_0.+)".toPattern()
-
     private val formatPattern = "(var\\s+(?:media|quality)_.+)".toPattern()
     private val mediaStringPattern = "(var.+?mediastring[^<]+)".toPattern()
     private val cleanRegex = "/\\*(?:(?!\\*/).)*?\\*/".toRegex()
     private val cleanVarRegex = "var\\s+".toRegex()
+    private val links = "https.+?(?=,upgrade)".toRegex()
+
 
     fun extractMediaString(page: String): String {
         val vars = hashMapOf<String, String>()
@@ -26,12 +25,19 @@ object Utils {
             vars[name] = parseSegment(value, vars)
         }
 
-        val formats = vars.filter { it.key.startsWith("media") || it.key.startsWith("quality_") }
+        val formats = vars.filter { it.key.startsWith("media") || it.key.startsWith("qualityItems_") }
 
-        return formats["quality_720p"]
-            ?: formats["quality_480p"]
-            ?: formats["quality_240p"]
-            ?: throw IllegalStateException("No formats detected")
+        val validFormats = formats.filter { it.key.startsWith("qualityItems")}
+        if(validFormats.isEmpty()) {
+            throw IllegalStateException("Failed to parse valid formats.")
+        } else if (validFormats.size > 1) {
+            throw IllegalStateException("Contains too many entries from parse.")
+        }
+
+        val newLink = validFormats.iterator().next().value.replace("\\", "", ignoreCase = true)
+
+        val values = links.findAll(newLink)
+        return values.iterator().next().value
     }
 
     fun extractAssignments(script: String): List<String> {
