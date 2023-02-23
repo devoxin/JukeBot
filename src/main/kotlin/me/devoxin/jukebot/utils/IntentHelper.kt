350c213c8a2532874ef2a94ed80ae0d91042c0ca
Devoxin <lukel9914@gmail.com>
2023-02-23 19:50:21 +0000

    disable message content

--- src/main/kotlin/me/devoxin/jukebot/utils/IntentHelper.kt
+++ src/main/kotlin/me/devoxin/jukebot/utils/IntentHelper.kt
@@ -1,26 +1,36 @@
 package me.devoxin.jukebot.utils
 
+import me.devoxin.jukebot.JukeBot
 import net.dv8tion.jda.api.requests.GatewayIntent
+import java.util.*
 
 object IntentHelper {
-    private val disabledIntents = listOf(
-        GatewayIntent.DIRECT_MESSAGES,
-        GatewayIntent.DIRECT_MESSAGE_REACTIONS,
-        GatewayIntent.DIRECT_MESSAGE_TYPING,
-        GatewayIntent.GUILD_MODERATION,
-        GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
-        GatewayIntent.GUILD_INVITES,
-        GatewayIntent.GUILD_MEMBERS,
-        GatewayIntent.GUILD_MESSAGE_REACTIONS,
-        GatewayIntent.GUILD_MESSAGE_TYPING,
-        GatewayIntent.GUILD_PRESENCES,
-        GatewayIntent.GUILD_WEBHOOKS,
-        GatewayIntent.SCHEDULED_EVENTS
-    )
+    private val disabledIntents: List<GatewayIntent> by lazy {
+        val intents = mutableListOf(
+            GatewayIntent.DIRECT_MESSAGES,
+            GatewayIntent.DIRECT_MESSAGE_REACTIONS,
+            GatewayIntent.DIRECT_MESSAGE_TYPING,
+            GatewayIntent.GUILD_MODERATION,
+            GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+            GatewayIntent.GUILD_INVITES,
+            GatewayIntent.GUILD_MEMBERS,
+            GatewayIntent.GUILD_MESSAGE_REACTIONS,
+            GatewayIntent.GUILD_MESSAGE_TYPING,
+            GatewayIntent.GUILD_PRESENCES,
+            GatewayIntent.GUILD_WEBHOOKS,
+            GatewayIntent.SCHEDULED_EVENTS
+        )
+
+        if (!JukeBot.isSelfHosted) {
+            intents.add(GatewayIntent.MESSAGE_CONTENT)
+        }
+
+        intents.toList()
+    }
 
     // Basically everything except GUILD_MESSAGES + GUILD_VOICE_STATES
 
-    private val disabledIntentsInt = GatewayIntent.getRaw(disabledIntents)
-    private val enabledIntentsInt = GatewayIntent.ALL_INTENTS and disabledIntentsInt.inv()
-    val enabledIntents = GatewayIntent.getIntents(enabledIntentsInt)
+    private val disabledIntentsInt: Int by lazy { GatewayIntent.getRaw(disabledIntents) }
+    private val enabledIntentsInt: Int by lazy { GatewayIntent.ALL_INTENTS and disabledIntentsInt.inv() }
+    val enabledIntents: EnumSet<GatewayIntent> by lazy { GatewayIntent.getIntents(enabledIntentsInt) }
 }
